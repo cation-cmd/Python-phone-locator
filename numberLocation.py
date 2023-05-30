7012b75504af9735f65bbe0faaeae97a5c20df5e
cation-cmd <52932894+cation-cmd@users.noreply.github.com>
2023-05-30 12:32:54 -0400

    Update numberLocation.py

--- numberLocation.py
+++ numberLocation.py
@@ -1,16 +1,3 @@
-""" import phonenumbers
-from test import number
-
-from phonenumbers import geocoder
-
-ch_nmber = phonenumbers.parse(number,"CH")
-print(geocoder.description_for_number(ch_number,"en"))
-
-from phonenumbers import carrier
-service_number """
-
-
-#NEW
 
 #numberLocation.py
 
